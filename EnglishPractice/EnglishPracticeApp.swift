371a94c8e56625235bad8b7514462ef34c3da2c2
Stephen Aleksei Kac Lozano <stephen@stephenkac.com>
2025-08-13 15:34:18 -0700

    Init

---
+++ EnglishPractice/EnglishPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnglishPracticeApp.swift
+//  EnglishPractice
+//
+//  Created by Admin on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct EnglishPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
